c80438f64d1576c53987d258678f8168c53801e7
Aliaksandr Baranouski <al.baranovski89@gmailcom>
2019-10-06 16:09:28 +0200

    Create project

---
+++ ReactDict/ReactDict/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ReactDict
+//
+//  Created by Aliaksandr Baranouski on 06/10/2019.
+//  Copyright © 2019 naschekrasche. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReactDict/ReactDict/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReactDict
+//
+//  Created by Aliaksandr Baranouski on 06/10/2019.
+//  Copyright © 2019 naschekrasche. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
